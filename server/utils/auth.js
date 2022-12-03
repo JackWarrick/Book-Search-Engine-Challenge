6186f1aa37d889d7ba08e464d8bdee5eefe0c0d5
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-12-03 15:46:25 -0600

    refactored auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,22 @@
 const jwt = require('jsonwebtoken');
 
+//Update auth middleware to work with graphQL API
+
+//I THINK THIS IS DONE
+
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+
+  //Took out res, and next from the params
+  authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+
+    //ADDED req.body.toekn
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +24,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +33,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
+    //Took out the next() function
     // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
